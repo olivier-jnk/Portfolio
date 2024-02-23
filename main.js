4110adda2e4552d007f5f25a408a7e289028c21d
olivier-jnk <oliviertrouvain01@gmail.com>
2024-02-23 13:39:40 +0100

    redirection vers la maquette Safrio

--- main.js
+++ main.js
@@ -1,3 +1,7 @@
 function page(pageNumber) {
     window.location.href = 'projets/projet' + pageNumber + '.html';
-}
+};
+
+function redirection() {
+    window.open('https://www.figma.com/file/U6JLy7Fip2xgQNy85Bifec/Safrio-mq-(PC)?type=design&node-id=0%3A1&mode=design&t=Y2pmtu5wYOa4DADz-1', '_blank');
+};
